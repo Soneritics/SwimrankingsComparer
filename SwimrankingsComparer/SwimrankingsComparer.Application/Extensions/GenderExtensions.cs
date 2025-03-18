cfa898eb2ea06d96bdadaed6b4ee94c9c522ee68
Jordi Jolink <mail@jordijolink.nl>
2025-03-18 08:35:50 +0100

    Extend filter

---
+++ SwimrankingsComparer/SwimrankingsComparer.Application/Extensions/GenderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using SwimRankings.Api.Models;
+
+namespace SwimrankingsComparer.Application.Extensions;
+
+public static class GenderExtensions
+{
+    public static string ToDutch(this Gender gender)
+    {
+        return gender switch
+        {
+            Gender.Male => "Man",
+            Gender.Female => "Vrouw",
+            _ => "Onbekend"
+        };
+    }
+}
